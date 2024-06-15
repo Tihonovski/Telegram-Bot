d744f79c0db4b1c6df293fd6a19e78a664422906
Slava Tihonovski <slavatihonovski@gmail.com>
2024-06-15 15:03:39 +0300

    Update python-telegram-bot version and adapt code to new API

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot.py
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from config import TELEGRAM_BOT_TOKEN
 from gpt_handler import gpt_response, detect_language
 
@@ -29,26 +29,20 @@ def error(update: Update, context: CallbackContext) -> None:
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main() -> None:
-    # יצירת האובייקט Updater
-    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
-
-    # קבלת ה-Dispatcher למתן גישה לרגיסטרציה של ה-handlers
-    dispatcher = updater.dispatcher
+    # יצירת האובייקט Application
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     # רישום הפונקציה start כ-handler לפקודת /start
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # רישום הפונקציה handle_message כ-handler להודעות טקסט
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # רישום הפונקציה error כ-handler לשגיאות
-    dispatcher.add_error_handler(error)
+    application.add_error_handler(error)
 
     # הפעלת הבוט
-    updater.start_polling()
-
-    # שמירה על הפעולה של הבוט עד להפסקה
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
