d64daacd6d0e92466f23213700446bc3965c50a4
Slava Tihonovski <slavatihonovski@gmail.com>
2024-06-15 14:43:29 +0300

    Add GitHub Actions workflow and config_template.py

--- config_template.py
+++ config_template.py
@@ -1,3 +1,4 @@
-# config_template.py
-TELEGRAM_BOT_TOKEN = 'your_api_key_here'
-OPENAI_API_KEY = 'your_bot_token_here'
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', 'your_api_key_here')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'your_bot_token_here')
